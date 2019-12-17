fe6d1295705ac26e97e6b96e4284c56b7728a16e
Wanderson Aldo <wanderson.aldo@gmail.com>
2019-12-17 09:37:21 -0200

    WebView properties

--- app/src/components/webview.js
+++ app/src/components/webview.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import { View, WebView, StyleSheet } from 'react-native';
+import { WebView, StyleSheet } from 'react-native';
 
 // import { Container } from './styles';
 
 export default function components() {
-  return (    
-    <WebView style={Styles.webview}></WebView>    
+  return (  
+    /** blog.parse.com */
+    /** Here
+     *  Here Mobile
+     */
+    <WebView style={Styles.webview}
+      source={{uri:'https://github.com/facebook/react-native'}}></WebView>    
   );
 }
 
 const Styles = StyleSheet.create({
     webview:{
         flex:1,
-        borderColor: 'blue'
+        borderColor: 'blue',
+        marginTop: 20
     }
 })
